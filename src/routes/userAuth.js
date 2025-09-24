23ad68e80cefabc7cccf5b6ff9594d7ee267313f
Samir Singh <ersamirsingh@gmail.com>
2025-09-24 21:29:05 +0530

    Routing fixed

--- src/routes/userAuth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { loginUser, registerUser } = require("../controllers/authController");
-
-const authRouter = express.Router();
-
-authRouter.post("/login", loginUser);
-authRouter.post("/register", registerUser); // only for testing
-
-module.exports = authRouter;
